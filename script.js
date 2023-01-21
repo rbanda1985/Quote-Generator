b6e6f23c01fb1f01a1bca0e6123f925cc48487df
rbanda1985 <robertbanda85@gmail.com>
2023-01-21 09:02:47 -0600

    ADD: Quotes from api to webpage

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote(){
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote)
+  // Check if Author field is blank and replace it with uknown
+
+  if(!quote.author){
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine styling
+
+  if(quote.text.length > 50){
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
+
+
 
 // Get Quotes from API
 async function getQuote(){
@@ -18,6 +44,16 @@ async function getQuote(){
         console.log(err)
       }
 }
+
+// Tweet Quote
+function tweetQuote(){
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
