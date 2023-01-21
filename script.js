055e30448d85eea28e9fcc259160ecac78c9caf2
rbanda1985 <robertbanda85@gmail.com>
2023-01-21 13:27:08 -0600

    Complete: Finished styling and the loader function

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter-button');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote(){
+  loading()
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author field is blank and replace it with uknown
@@ -25,16 +39,19 @@ function newQuote(){
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set Quote Hide Loader
 
 
   authorText.textContent = quote.author;
   quoteText.textContent = quote.text;
+  complete()
 }
 
 
 
 // Get Quotes from API
 async function getQuote(){
+  loading()
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
       try{
         const response = await fetch(apiUrl)
